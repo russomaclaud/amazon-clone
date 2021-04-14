ad608c8381bac4594deed337b0d32d41c4752943
russomaclaud <erussomaclaud@gmail.com>
2021-04-14 16:04:30 -0500

    Navbar listo

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXQTbgexUGQRinoy4_XUeroUJT6qfipb4",
+  authDomain: "clone-3c90e.firebaseapp.com",
+  projectId: "clone-3c90e",
+  storageBucket: "clone-3c90e.appspot.com",
+  messagingSenderId: "117961388845",
+  appId: "1:117961388845:web:0605a959cd14d137b459a4",
+  measurementId: "G-W8VWSNV0JK"
+};
